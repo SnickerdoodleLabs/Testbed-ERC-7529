15c9bcf0a7cdbeb2cd960d46c934b4aff62419f3
TtheBC01 <tac688@alumni.stanford.edu>
2023-11-07 11:10:27 -0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "reflect-metadata";
 import './App.css';
 import './Footer';
+import logo from './snickerdoodle_horizontal_notab.png'
 import Footer from './Footer';
 import DomainInput from './DomainInput';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div className='App-body'>
+        <img src={logo} className="App-logo" alt="logo" /><br></br>
         <div>
           Search for contracts by domain name with <a href='https://www.npmjs.com/package/@snickerdoodlelabs/erc7529'>ERC-7529</a>!
         </div><br />
