cdc4359a4922e2e90049c3e85bcce1ae8f0c9c39
TtheBC01 <tac688@alumni.stanford.edu>
2023-11-09 13:54:43 -0800

    update

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,8 +4,8 @@ import logo from './snickerdoodle_horizontal_notab.png'
 export default function Header() {
     return (
         <header className="header">
-            <a href='https://snickerdoodle.com'>
-                <img src={logo} className="header-logo" alt="logo" />
+            <a href='https://snickerdoodle.com' className='header'>
+                <img src={logo} alt="logo" className="header-logo"/>
             </a>
         </header>
     );
